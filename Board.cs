64f7333366c8d793ab2e84d7454cb99207fd81f0
ReanSchwarzer1 <42261295+ReanSchwarzer1@users.noreply.github.com>
2019-08-16 00:37:48 +0530

    Add files via upload

---
+++ Board.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    // uniform distance between nodes
+    public static float spacing = 2f;
+
+    // four compass directions
+    public static readonly Vector2[] directions =
+    {
+        new Vector2(spacing, 0f),
+        new Vector2(-spacing, 0f),
+        new Vector2(0f, spacing),
+        new Vector2(0f, -spacing)
+    };
+
+    // list of all of the Nodes on the Board
+    List<Node> m_allNodes = new List<Node>();
+    public List<Node> AllNodes { get { return m_allNodes; } }
+
+    // the Node directly under the Player
+    Node m_playerNode;
+    public Node PlayerNode { get { return m_playerNode; }}
+
+    // the PlayerMover component
+    PlayerMover m_player;
+
+    void Awake()
+    {
+        m_player = Object.FindObjectOfType<PlayerMover>().GetComponent<PlayerMover>();
+        GetNodeList();
+    }
+
+    // sets the AllNodes and m_allNodes fields
+    public void GetNodeList()
+    {
+        Node[] nList = Object.FindObjectsOfType<Node>();
+        m_allNodes = new List<Node>(nList);
+    }
+
+    // returns a Node at a given position
+    public Node FindNodeAt(Vector3 pos)
+    {
+        Vector2 boardCoord = Utility.Vector2Round(new Vector2(pos.x, pos.z));
+        return m_allNodes.Find(n => n.Coordinate == boardCoord);
+
+    }
+
+    // return the PlayerNode
+    public Node FindPlayerNode()
+    {
+        if (m_player != null && !m_player.isMoving)
+        {
+            return FindNodeAt(m_player.transform.position);
+        }
+        return null;
+    }
+
+    // set the m_playerNode
+    public void UpdatePlayerNode()
+    {
+        m_playerNode = FindPlayerNode();
+    }
+
+    // draw a colored sphere at the PlayerNode
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0f, 1f, 1f, 0.5f);
+        if (m_playerNode != null)
+        {
+            Gizmos.DrawSphere(m_playerNode.transform.position, 0.2f);
+        }
+    }
+
+}
